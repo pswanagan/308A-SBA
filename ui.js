9a908d811e39b69db626a1738e42dcc294724d78
pswanagan <pswanagan@gmail.com>
2024-01-05 15:08:49 -0600

    updated the way that info was presented on card

--- ui.js
+++ ui.js
@@ -17,9 +17,42 @@ function updateUI(dogs) {
                 </div>
             </div>
         `;
+         // Attach click event listener to each card
+         element.addEventListener('click', () => {
+            showDogInfo(dog);
+        });
 
         dogList.appendChild(element);
     });
 }
 
 
+
+function showDogInfo(dog) {
+    const modalTitle = document.getElementById('dogInfoModalLabel');
+    const modalBody = document.querySelector('#dogInfoModal .modal-body');
+    const dogOrigin = dog.origin ? dog.origin: 'N/A';
+    const dogBreedGroup = dog.breed_group ? dog.breed_group : 'N/A';
+    const dogBredFor = dog.bred_for ? dog.bred_for : 'N/A';
+    const dogTemperament = dog.temperament ? dog.temperament : 'N/A';
+    const dogLifeSpan = dog.life_span ? dog.life_span : 'N/A';
+    const dogHeight = dog.height.imperial ? dog.height.imperial : 'N/A';
+    const dogWeight = dog.weight.imperial ? dog.weight.imperial : 'N/A';
+
+    // Populate modal with dog information
+    modalTitle.textContent = dog.name;
+    modalBody.innerHTML = `
+        <img src="${dog.image.url}" class="img-fluid mb-3" alt="${dog.name}">
+        <p>Breed: ${dog.name}</p>
+        <p>Breed Group: ${dogBreedGroup}</p>
+        <p>Origin: ${dogOrigin}</p>
+        <p>Bred For: ${dogBredFor}</p>
+        <p>Temperament: ${ dogTemperament}</p>
+        <p>Life Span: ${dogLifeSpan}</p>
+        <p>Height: ${dogHeight} inches</p>
+        <p>Weight: ${dogWeight} lbs</p>
+    `;
+
+    // Show the modal
+    $('#dogInfoModal').modal('show');
+}
